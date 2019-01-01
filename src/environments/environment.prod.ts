8d17e52785de40549c0ebb3a9a158be21927e73e
kalyanteja <kalyansmacks@gmail.com>
2019-01-02 03:05:39 +0530

    added pwa support

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  LASTFM_API_URL: 'http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&format=json&api_key=',
+  LASTFM_API_KEY: '12d5ddb92cc3bfaefd9d016e488d0cd8'
 };
